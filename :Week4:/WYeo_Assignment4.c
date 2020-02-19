70f1205d659f3696221c19c4d905fecb9722acfc
willyeo2 <wyeo@berklee.edu>
2020-02-18 20:26:33 -0500

    Simplified

--- :Week4:/WYeo_Assignment4.c
+++ :Week4:/WYeo_Assignment4.c
@@ -3,12 +3,11 @@
 int main(){
   for (int i = 0; i < 8 ; i++)
   {
-      for (int j = 0 ; j < i
-        + 1 ; j++)
+      for (int j = 0 ; j < 1 ; j++)
       {
       printf("#");
       }
+
   printf("\n");
   }
-
 }
