477ee5f5183a45bea48a0527080c6f7047c65dd8
willyeo2 <wyeo@berklee.edu>
2020-02-18 20:21:38 -0500

    Week 4 Assignment

---
+++ :Week4:/WYeo_Assignment4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  for (int i = 0; i < 8 ; i++)
+  {
+      for (int j = 0 ; j < i
+        + 1 ; j++)
+      {
+      printf("#");
+      }
+  printf("\n");
+  }
+
+}
