6784af119922f8c195844dd1b0ef5542bc9d8e2b
willyeo2 <wyeo@berklee.edu>
2020-03-09 07:20:10 -0400

    Will Edit

--- :Week6:/WYeo_Assignment6.js
+++ :Week6:/WYeo_Assignment6.js
@@ -1,10 +1,12 @@
+let size = 8;
+let string = "";
 for (let i = 0; i < size; i++){
     for (let j = 0; j < size; j++){
-    if ((i + j) % 2 == 0)
-        space += " ";
-    else
-        space += "#";
-    }
-    space += "\n"};
-
-console.log(space)
+        if ((i + j) % 2 == 0){
+            string += " ";
+        } else {
+            string += "#";
+        }
+    } string += "\n";
+}
+console.log(string)
